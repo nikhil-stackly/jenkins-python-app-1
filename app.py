f3f2227b95dd43cce47c3cb5cb3709b93771eb30
Ubuntu <ubuntu@ip-172-31-31-169.ec2.internal>
2026-01-02 12:12:18 +0000

    Auto deploy test

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Jenkins CI/CD 🚀"
+    return "Auto Deploy Works 🚀"
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
