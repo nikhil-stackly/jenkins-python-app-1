65cae76ef491591530222a2354d0810c213fc233
Nikhil Matthew <nikhilmatthew111@gmail.com>
2026-01-21 14:27:00 +0530

    webhook test

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Auto Deploy Works 🚀"
+@app.route("/health")
+def health():
+    return {"status": "Auto Deploy Works 🚀 from Webhook"}
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host="0.0.0.0", port=5000)
